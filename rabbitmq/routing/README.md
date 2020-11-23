e39f50ccca34b2850f0a5c472a15a90cb5ed83bd
sergeyyurkin <syyurkin@gmail.com>
2020-11-24 02:54:00 +0700

    added routing

---
+++ rabbitmq/routing/README.md
@@ -0,0 +1,5 @@
+## Run RabbitMQ in Docker
+
+```
+docker run --rm -it --hostname my-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+```
---
+++ rabbitmq/routing/Receiver/ReceiveLogDirect.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receiver
+{
+    internal class ReceiveLogDirect
+    {
+        private static int Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.ExchangeDeclare("direct_logs", "direct");
+            var queueName = channel.QueueDeclare().QueueName;
+
+            if (args.Length < 1)
+            {
+                Console.Error.WriteLine("Usage: {0} [Info] [Warning] [Error]", Environment.GetCommandLineArgs()[0]);
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+                return 1;
+            }
+
+            foreach (var severity in args)
+            {
+                channel.QueueBind(queueName, "direct_logs", severity);
+            }
+
+            Console.WriteLine(" [*] Waiting for messages.");
+
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                var routingKey = ea.RoutingKey;
+                Console.WriteLine(" [x] Received '{0}':'{1}'", routingKey, message);
+            };
+
+            channel.BasicConsume(queueName, true, consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+            return 0;
+        }
+    }
+}
